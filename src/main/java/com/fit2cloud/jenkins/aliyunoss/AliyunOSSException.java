b3d41ab79da89d1f17a4362d967746956078cd64
Jianjin Fan <fanjianjin@gmail.com>
2015-08-06 15:44:52 +0800

    Reformat code.
    Signed-off-by: Jianjin Fan <fanjianjin@gmail.com>

--- src/main/java/com/fit2cloud/jenkins/aliyunoss/AliyunOSSException.java
+++ src/main/java/com/fit2cloud/jenkins/aliyunoss/AliyunOSSException.java
@@ -2,17 +2,17 @@ package com.fit2cloud.jenkins.aliyunoss;
 
 public class AliyunOSSException extends Exception {
 
-	private static final long serialVersionUID = 1582215285822395979L;
+    private static final long serialVersionUID = 1582215285822395979L;
 
-	public AliyunOSSException() {
-		super();
-	}
+    public AliyunOSSException() {
+        super();
+    }
 
-	public AliyunOSSException(final String message, final Throwable cause) {
-		super(message,cause); 
-	}
+    public AliyunOSSException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 
-	public AliyunOSSException(final String message) {
-		super(message);
-	}
+    public AliyunOSSException(final String message) {
+        super(message);
+    }
 }
--- src/main/java/com/fit2cloud/jenkins/aliyunoss/Utils.java
+++ src/main/java/com/fit2cloud/jenkins/aliyunoss/Utils.java
@@ -4,30 +4,30 @@ import java.io.IOException;
 import java.util.Map;
 
 import hudson.Util;
-import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
 
 
 public class Utils {
-	public static final String FWD_SLASH = "/";
-	
-	public static boolean isNullOrEmpty(final String name) {
-		boolean isValid = false;
-		if (name == null || name.matches("\\s*")) {
-			isValid = true;
-		}
-		return isValid;
-	}
-	
-	public static String replaceTokens(AbstractBuild<?, ?> build,
-			BuildListener listener, String text) throws IOException,
-			InterruptedException {
-		String newText = null;
-		if (!isNullOrEmpty(text)) {
-			Map<String, String> envVars = build.getEnvironment(listener);
-			newText = Util.replaceMacro(text, envVars);
-		}
-		return newText;
-	}
+
+    public static final String FWD_SLASH = "/";
+
+    public static boolean isNullOrEmpty(final String name) {
+        boolean isValid = false;
+        if (name == null || name.matches("\\s*")) {
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    public static String replaceTokens(AbstractBuild<?, ?> build, BuildListener listener,
+                                       String text) throws IOException, InterruptedException {
+        String newText = null;
+        if (!isNullOrEmpty(text)) {
+            Map<String, String> envVars = build.getEnvironment(listener);
+            newText = Util.replaceMacro(text, envVars);
+        }
+        return newText;
+    }
 
 }
